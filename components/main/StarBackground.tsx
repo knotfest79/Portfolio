f1254b044d904bb65808d162e821e18366ccdfa1
asterik <vikingsfortune@gmail.com>
2025-04-09 18:26:18 +0930

    poject deployment

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -1,50 +1,50 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-"use client";
+// /* eslint-disable @typescript-eslint/ban-ts-comment */
+// "use client";
 
-import { useState, useRef, Suspense } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Points, PointMaterial } from "@react-three/drei";
+// import { useState, useRef, Suspense } from "react";
+// import { Canvas, useFrame } from "@react-three/fiber";
+// import { Points, PointMaterial } from "@react-three/drei";
 
-//@ts-ignore
-import * as random from "maath/random/dist/maath-random.esm";
+// //@ts-ignore
+// import * as random from "maath/random/dist/maath-random.esm";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const StarBackground = (props: any) => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const ref: any = useRef("");
+// // eslint-disable-next-line @typescript-eslint/no-explicit-any
+// const StarBackground = (props: any) => {
+//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//   const ref: any = useRef("");
 
-  const [sphere] = useState(() => {
-    const arr = new Float32Array(15000); // 5000 points * 3 coords each
-    return random.inSphere(arr, { radius: 1.2 });
-  });
+//   const [sphere] = useState(() => {
+//     const arr = new Float32Array(15000); // 5000 points * 3 coords each
+//     return random.inSphere(arr, { radius: 1.2 });
+//   });
 
-  useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10;
-    ref.current.rotation.y -= delta / 15;
-  });
+//   useFrame((state, delta) => {
+//     ref.current.rotation.x -= delta / 10;
+//     ref.current.rotation.y -= delta / 15;
+//   });
 
-  return (
-    <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
-        <PointMaterial
-          transparent
-          color="#ffffff"
-          size={0.002}
-          sizeAttenuation={true}
-          depthWrite={false} //
-        />
-      </Points>
-    </group>
-  );
-};
+//   return (
+//     <group rotation={[0, 0, Math.PI / 4]}>
+//       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+//         <PointMaterial
+//           transparent
+//           color="#ffffff"
+//           size={0.002}
+//           sizeAttenuation={true}
+//           depthWrite={false} //
+//         />
+//       </Points>
+//     </group>
+//   );
+// };
 
-const StarsCanvas = () => (
-  <div className="w-full h-auto fixed inset-0 z-[20]">
-    <Canvas camera={{ position: [0, 0, 1] }}>
-      <Suspense fallback={null}>
-        <StarBackground />
-      </Suspense>
-    </Canvas>
-  </div>
-);
-export default StarsCanvas;
+// const StarsCanvas = () => (
+//   <div className="w-full h-auto fixed inset-0 z-[20]">
+//     <Canvas camera={{ position: [0, 0, 1] }}>
+//       <Suspense fallback={null}>
+//         <StarBackground />
+//       </Suspense>
+//     </Canvas>
+//   </div>
+// );
+// export default StarsCanvas;
